f4817b133a1fd83f36641850baea47ee0628099f
Siby Abin Thomas <sibyoncloud@gmail.com>
2023-08-17 19:04:47 +0530

    adding the latest changes

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,16 +2,7 @@
 set -e
 
 active_exercises="101 102 103"
-
 echo "INFO:Init Script Started"
-
-# GRANT ALL PRIVILEGES ON DATABASE practise TO dbuser;
-# \connect practise;
-# create schema if not exists solutions;
-# create schema if not exists test;
-# grant all on schema solutions to dbuser;
-# grant all on schema test to dbuser;
-# set search_path to solutions, test, public;
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     create user dbuser with password '$POSTGRES_PASSWORD';
@@ -21,10 +12,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     grant connect on database practise to dbuser;
     show search_path;
 EOSQL
-
-# psql -v --username dbuser --dbname practise -f /scripts/exercise-101/sample.sql
-# psql -v --username dbuser --dbname practise -f /scripts/exercise-102/1-setup.sql
-# psql -v --username dbuser --dbname practise -f /scripts/exercise-103/1-setup.sql
 
 for ex in $active_exercises
 do 
